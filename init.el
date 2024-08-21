4b2a4e6b2986e25b6d3164eee7d2c921462a1c6b
ardeleanasm <ardeleanasm@gmail.com>
2024-08-21 19:07:28 +0300

    emacs update

--- init.el
+++ init.el
@@ -1,21 +1,11 @@
 (org-babel-load-file "~/.emacs.d/configuration.org")
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default default default italic underline success warning error])
- '(custom-enabled-themes '(nord))
- '(custom-safe-themes
-   '("37768a79b479684b0756dec7c0fc7652082910c37d8863c35b702db3f16000f8" default))
- '(indent-tabs-mode nil)
- '(menu-bar-mode t)
- '(package-selected-packages
-   '(elfeed slime rust-mode ## ecb quelpa tree-sitter-langs tree-sitter typescript-mode python-mode anaconda-mode lsp-haskell haskell-mode clojure-mode yasnippet-snippets nord-theme))
- '(tab-always-indent t)
- '(tool-bar-mode t)
- '(warning-suppress-types '((comp) (comp) (comp) (comp))))
+ '(package-selected-packages '(nord-theme)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
